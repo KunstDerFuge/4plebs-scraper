7e9a6a033d977e17a65cc2de5acf475b86fb4e3c
robert <robert@ferretech.io>
2021-04-18 16:47:13 -0700

    Add automatic pagination to search function

--- search.py
+++ search.py
@@ -1,8 +1,18 @@
 import json
 import requests
+import time
 
 
 def search4plebs():
+
+    api_url_base = "http://archive.4plebs.org/_/api/chan/search/"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    }
+    
+    def run_search(params, headers):
+        response = requests.get(api_url_base, params=params, headers=headers)
+        return response.json()
 
     with open('search_params.json', 'r') as params_file:
         params_string = params_file.read()
@@ -13,17 +23,27 @@ def search4plebs():
 
     print('Running search...')
 
-    api_url_base = "http://archive.4plebs.org/_/api/chan/search/"
+    page = 1
+    posts = None
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
-    }
-
-    response = requests.get(api_url_base, params=params, headers=headers)
+    while True:
+        print('Fetching results for page {}...'.format(page))
+        params['page'] = page
+        results = run_search(params, headers)
+        if 'error' in results:
+            break
+        if not posts:
+            posts = results
+        else:
+            posts['0']['posts'].extend(results['0']['posts'])
+        page += 1
+        print('Waiting 12 seconds to comply with rate limiting...')
+        time.sleep(12)
+    
     print('Search complete! \nWriting to {}...'.format(output))
 
     with open(output, 'w') as f:
-        f.write(json.dumps(response.json(), indent=4))
+        f.write(json.dumps(posts, indent=4))
 
     print('Wrote to {}!'.format(output))
 
